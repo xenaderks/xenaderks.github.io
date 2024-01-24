03d9e0030de50bee757ae89ca5cc240b716b8b2e
xenaderks <xenaderks@gmail.com>
2024-01-24 09:47:00 +0100

    Large UI update
    Mostly edits to the title page, adding buttons. Also made a new class to make adding elements like rows of buttons easier.

--- sketch.js
+++ sketch.js
@@ -15,9 +15,12 @@ let siteState = 0;
 let maxSiteState = 3;
 let doStates = true; // when presenting content I want to temporarily stop the state machine.
 let links = {}; // used for menu scene -> check setup
+let metaBlocks = {};
 let mouseContext = false;
 let startX, startY;
 let touchStartTime;
+let topText = "";
+let bottomText = "";
 
 // C O L O R S
 let backCol;
@@ -56,20 +59,8 @@ let directionText = document.getElementById('direction');
 
 // FUNC MOUSEWHEEL
 function mouseWheel(e) {
-	let threshold = 40;
 	if (canScroll && doStates) {
-		if(e.deltaY > threshold) { // down
-			if (siteState == maxSiteState) { return(false); } // prevents safari from throwing a fit.
-			transitions.next_scene.progress = 0;
-			transitions.next_scene.tGoal = siteState + 1;
-			scrollDir = "DOWN"; canScroll = false;
-		}
-		if(e.deltaY < -threshold) { // up
-			if (siteState == 0) { return(false); } // prevents safari from throwing a fit.
-			transitions.prev_scene.progress = 0;
-			transitions.prev_scene.tGoal = siteState - 1;
-			scrollDir = "UP"; canScroll = false;
-		}
+		transitionTrigger(e.deltaY);
 	}
 	return(false); // prevents safari from throwing a fit.
 }; 
@@ -83,20 +74,9 @@ function touchStarted() {
 function touchEnded() {
 	let eY = startY - mouseY;
 	let timeDiff = millis() - touchStartTime;
-	let threshold = 40;
+	if (hitBoxCheck()) { return(false) }
 	if (canScroll && doStates && timeDiff < 200) {
-		if(eY > threshold) { // down
-			if (siteState == maxSiteState) { return(false); } // prevents safari from throwing a fit.
-			transitions.next_scene.progress = 0;
-			transitions.next_scene.tGoal = siteState + 1;
-			scrollDir = "DOWN"; canScroll = false;
-		}
-		if(eY < -threshold) { // up
-			if (siteState == 0) { return(false); } // prevents safari from throwing a fit.
-			transitions.prev_scene.progress = 0;
-			transitions.prev_scene.tGoal = siteState - 1;
-			scrollDir = "UP"; canScroll = false;
-		}
+		transitionTrigger(eY);
 	}
 	return(false); // prevents safari from throwing a fit.
 }
@@ -138,26 +118,56 @@ function setup() {
 										createVector(0,40*th), 
 										"game\nmusic",
 										true,
-										gameImage);
+										gameImage,
+										col1 = color(0, 0, 0, 0),
+										col2 = color(255, 255, 255, 255),
+										col3 = color(255, 255, 255, 100),
+										col4 = color(0, 0, 0, 255),
+										underlineOrHighlight = "HIGHLIGHT",
+										pageLink = "index.html");
 	links.theater = new content_rect(333*tw, 646*th, 
 										createVector(666*tw,40*th), 
 										"musical\ntheatre",
 										true,
-										theaterImage);
+										theaterImage,
+										col1 = color(0, 0, 0, 0),
+										col2 = color(255, 255, 255, 255),
+										col3 = color(255, 255, 255, 100),
+										col4 = color(0, 0, 0, 255),
+										underlineOrHighlight = "HIGHLIGHT",
+										pageLink = "index.html");
 	links.standalones = new content_rect(666*tw, 293*th, 
 										createVector(333*tw,666*th), 
 										"standalone\ncompositions",
 										true,
-										standalonesImage);
+										standalonesImage,
+										col1 = color(0, 0, 0, 0),
+										col2 = color(255, 255, 255, 255),
+										col3 = color(255, 255, 255, 100),
+										col4 = color(0, 0, 0, 255),
+										underlineOrHighlight = "HIGHLIGHT",
+										pageLink = "index.html");
 	links.game_design = new content_rect(333*tw, 626*th, 
 										createVector(0,333*th), 
 										"game\ncredits",
 										true,
-										programmingImage);
+										programmingImage,
+										col1 = color(0, 0, 0, 0),
+										col2 = color(255, 255, 255, 255),
+										col3 = color(255, 255, 255, 100),
+										col4 = color(0, 0, 0, 255),
+										underlineOrHighlight = "HIGHLIGHT",
+										pageLink = "index.html");
 	links.other_art = new content_rect(333*tw, 333*th, 
 										createVector(333*tw,333*th),
 										"other\nart",
 										true)
+	metaBlocks.titlePage = new metaContainer(	createVector(850*tw, 65*tw),
+												createVector(75*tw, 350*th),
+												"DEMO REEL",
+												"MY WORK",
+												"ABOUT ME",
+												"CONTACT INFO");
 }
 // END SETUP FUNC
 
@@ -176,23 +186,26 @@ function draw() {
 	switch (siteState) {
 		case 0:
 			text_preset(0); text('XENA', tw*500, th*400);
-			text_preset(1); text('scroll to continue', tw*500, th*750);
+			topText = "";
+			bottomText = "m y   w o r k";
+			if (a <= 1.2) { metaBlocks.titlePage.pos.y = th*400 + textAscent('XENA') }
+			else if (a <= 1.45) { metaBlocks.titlePage.pos.y = tw*280 + textAscent('XENA') }
+			else { metaBlocks.titlePage.pos.y = th*200 + textAscent('XENA') }
+			metaBlocks.titlePage.testHover();
+			metaBlocks.titlePage.place();
 			break;
 		case 1:
-			textSize(20*th);
-			noStroke();
-			fill(255,255,255,255);
-			text("t i t l e   s c r e e n", tw*500, th*15);
+			textAlign(CENTER, CENTER);
 			for (let key in links) {
-				links[key].hover_test();
+				links[key].testHover();
 				links[key].place();
 			}
-			textSize(20*th);
-			noStroke();
-			fill(255,255,255,255);
-			text("a b o u t   m e", tw*500, th*975);
+			topText = "t h e   t i t l e   p a g e"
+			bottomText = "m o r e   a b o u t   m e";
 			break;
 		case 2:
+			topText = "m y   w o r k";
+			bottomText = "m y   c o n t a c t - i n f o";
 			let aboutMeText = "Hi there! Welcome to my portfolio. I'm a 23-year-old music and technology student at the University for the arts in Utrecht, where I'll be graduating in may. I have a passion for multidisciplinary projects, especially video games and theatre, and use my skills in programming to supplement my skills in composition on musical performance.\n\n" +
 			"My specialty is in temperate, methodical music. I like odd time signatures and keys; ideally my audience is on the edge of their seats at all times! Arrangement isn't a major concern, as I'm as well versed in working together with acoustic musicians as I am with sampling, synths and virtual orchestration. Where a faster pace is required I don't struggle to adjust, though I tend to work a little slower in relation to my lack of experience.\n\n"+
 			"Where possible I prefer to work in a team, face to face- though most of my projects end up being more practically served by online cooperation. To me there's nothing more exciting than sitting with my team and discussing the next steps we're going to be taking in our project.";
@@ -203,13 +216,25 @@ function draw() {
 				text(aboutMeText, tw*20, th*250, tw*550);
 				image(meImage, tw*600, th*100, tw*300, th*820);
 			} else { // PORTRAIT
-				text_preset(0); textSize(tw*180); strokeWeight(5);
-				text('about me', tw*500, th*150);
-				text_preset(1); textAlign(LEFT, TOP); textSize(th*20);
+				text_preset(0); textSize(tw*170); strokeWeight(5);
+				text('about me', tw*500, th*140);
+				text_preset(1); textAlign(LEFT, TOP); textSize(th*18);
 				text(aboutMeText, tw*40, 450*tw, tw*920);
 			}
+			textAlign(CENTER, CENTER);
 			break;
 	}
+	// VV Top + Bottom text
+	textAlign(CENTER, CENTER);
+	text_preset(3);
+	let instruction;
+	let upInstruction = "";
+	let downInstruction = "";
+	if (isTouch) { instruction = "s c r o l l   "; } else { instruction = "s w i p e   ";};
+	if (siteState != maxSiteState) { upInstruction = instruction + "u p   f o r   " + bottomText; }
+	if (siteState != 0) { downInstruction = instruction + "d o w n   f o r   " + topText; }
+	text(upInstruction, tw*500, th*967);
+	text(downInstruction, tw*500, th*15);
 	// VV Manage Transitions VV
 	if (canScroll) { return }
 	switch (scrollDir) {
@@ -257,15 +282,72 @@ function windowResized() {
 	links.other_art = new content_rect(333*tw, 333*th, 
 										createVector(333*tw,333*th),
 										"other\nart",
-										true)
+										true);
+	metaBlocks.titlePage = new metaContainer(	createVector(850*tw, 90*th),
+												createVector(75*tw, 600*th),
+												"DEMO REEL",
+												"MY WORK",
+												"ABOUT ME",
+												"CONTACT INFO");
 }
+// END WINDOW RESIZED
 
+// FUNC TESTRECTHOVER
+function testRectHover(point1, point2) {
+	if (mouseX < point2.x && mouseX > point1.x &&
+		mouseY < point2.y && mouseY > point1.y) {
+		return(true);
+	}
+	return(false);
+}
+// END TESTRECTHOVER
 
 // C - U - S - T - O - M --------------------------------------------------------
 //	F - U - N - C - S -----------------------------------------------------------
 
+// FUNC TRANSITION TRIGGER
+function transitionTrigger(triggerValue, threshold = 40, stateSetter = 999) {
+	if(triggerValue > threshold) { // down
+		if (siteState == maxSiteState) { return(false) } // prevents safari from throwing a fit.
+		transitions.next_scene.progress = 0;
+		if (stateSetter == 999) { transitions.next_scene.tGoal = siteState + 1 }
+		else { transitions.next_scene.tGoal = stateSetter }
+		scrollDir = "DOWN"; canScroll = false;
+	}
+	if(triggerValue < -threshold) { // up
+		if (siteState == 0) { return(false) } // prevents safari from throwing a fit.
+		transitions.prev_scene.progress = 0;
+		if (stateSetter == 999) { transitions.prev_scene.tGoal = siteState - 1 }
+		else { transitions.prev_scene.tGoal = stateSetter }
+		scrollDir = "UP"; canScroll = false;
+	}
+}
+// END TRANSITION TRIGGER FUNC
+
+// FUNC HITBOX CHECK
+function hitBoxCheck() {
+	for (let key in metaBlocks) {
+		let r = metaBlocks[key].rects;
+		for (let keyception in r) {
+			if (r[keyception].testHover()) {
+				transitionTrigger(1, 0, metaBlocks[key].ssll[keyception]);
+				return(true);
+			}
+		}
+	}
+	for (let key in links) {
+		if (links[key].testHover() && links[key].url != "") {
+			let newPage = links[key].url;
+			window.open(newPage, "_blank");
+			return(true);
+		}
+	}
+}
+// END HITBOX CHECK
+
 // FUNC TEXT PRESET
-function text_preset(setter) {
+function text_preset(setter, mod = false) {
+	// mod is a modifier. Triggers some special properties.
 	switch (setter) {
 		case 0: // XENA text
 			fill(main_pink);
@@ -280,8 +362,13 @@ function text_preset(setter) {
 			break;
 		case 2: // content-rect text.
 			textFont(mainFont);
-			if (a > 1) { textSize(tw*30) } else { textSize(tw*50) };
+			if (a > 0.7 || mod) { textSize(tw*30) } else { textSize(tw*50) };
 			break;
+		case 3: // top + bottom text;
+			textFont(mainFont);
+			textSize(20*th);
+			noStroke();
+			fill(255,255,255,255);
 	}
 }
 // END TEXT PRESET FUNC
@@ -397,31 +484,37 @@ class content_rect {
 				rect_pos,
 				rect_text,
 				do_outline = true,
-				rect_img = "") {
-		this.w = rect_width;
-		this.h = rect_height;
+				rect_img = "",
+				col1 = color(0, 0, 0, 0),
+				col2 = color(255, 255, 255, 255),
+				col3 = color(255, 255, 255, 100),
+				col4 = color(0, 0, 0, 255),
+				underlineOrHighlight = "HIGHLIGHT",
+				pageLink = "") {
+		this.url = pageLink;
+		this.w = rect_width; 	this.h = rect_height;
+		this.dims = createVector(this.w, this.h);
 		this.pos = rect_pos;
 		this.txt = rect_text;
 		this.o = do_outline;
-		this.color1 = color(0, 0, 0, 0);
-		this.color2 = color(255, 255, 255, 255);
-		this.color3 = color(255, 255, 255, 100);
-		this.color4 = color(0, 0, 0, 255);
+		this.color1 = col1; 	this.color2 = col2;
+		this.color3 = col3; 	this.color4 = col4;
 		this.hovered = false;
+		this.mod = false;
 		this.lerp_state = 0;
 		this.img = rect_img;
+		this.uoh = underlineOrHighlight;
 		this.transer = new transition("IN-OUT",
 									"LINEAR",
 									80,
 									4,
 									[	[this.pos.x, this.pos.y], 
-										[this.pos.x + this.pos.x, this.pos.y + this.pos.y]
+										[this.pos.x + this.w, this.pos.y + this.h]
 									])
 	}
 	place() {
 		let rect_col = lerpColor(this.color1, this.color3, this.lerp_state);
 		let txt_col = lerpColor(this.color4, this.color2, 1.0 - this.lerp_state);
-		textStyle(BOLD);
 		// draw the rectangle itself.
 		if (this.img != "") { 
 			// image(this.img, this.pos.x, this.pos.y);
@@ -434,29 +527,109 @@ class content_rect {
 					CENTER, CENTER);
 		}
 		fill(rect_col);
-		strokeWeight(5);
-		if (!this.o) { noStroke } else { stroke(this.color2) };
+		strokeWeight(5*tw);
+		if (!this.o) { noStroke() } else { stroke(this.color2) };
 		rect(this.pos.x, this.pos.y, this.w, this.h);
 		// draw the text.
-		text_preset(2);
+		text_preset(2, this.mod);
 		fill(txt_col);
 		stroke(txt_col);
 		strokeWeight(tw*1.75);
 		text(this.txt, this.pos.x + this.w / 2, this.pos.y + this.h / 2);
 	}
-	hover_test() {
-		if (winMouseX < this.pos.x + this.w && winMouseX > this.pos.x &&
-			winMouseY < this.pos.y + this.h && winMouseY > this.pos.y) {
+	testHover() {
+		if (testRectHover(this.pos, createVector(this.dims.x+this.pos.x, this.dims.y+this.pos.y))) {
 			this.hovered = true;
+			if (this.uoh == "UNDERLINE") {
+				this.o = true;
+				return(true);
+			}
 			if (this.lerp_state < 1.0) {
 				this.lerp_state += 0.08;
 			}
-		} else {
-			this.hovered = false;
-			if (this.lerp_state > 0.0) {
-				this.lerp_state -= 0.08;
-			}
+			return(true);
 		}
-		return (this.hovered);
+		this.hovered = false;
+		if (this.uoh == "UNDERLINE") {
+			this.o = false;
+			return(false);
+		}
+		if (this.lerp_state > 0.0) {
+			this.lerp_state -= 0.08;
+		}
+		return(false);
 	}
 }
+
+// CLASS META CONTAINER
+class metaContainer {
+	constructor(metaDimensions,
+				metaPos,
+				rect1text = "", rect2text = "",
+				rect3text = "", rect4text = "",
+				col1 = color(0, 0, 0, 0),
+				col2 = color(255, 255, 255, 255),
+				col3 = color(255, 255, 255, 100),
+				col4 = color(0, 0, 0, 255),
+				underlineOrHighlight = 'UNDERLINE',
+				siteStatesLinksList = [3, 1, 2, 5]) {
+		this.ssll = siteStatesLinksList;
+		this.dims = metaDimensions;
+		this.pos = metaPos;
+		this.txt1 = rect1text; this.txt2 = rect2text;
+		this.txt3 = rect3text; this.txt4 = rect4text;
+		this.texts = [this.txt1, this.txt2, this.txt3, this.txt4];
+		this.color1 = col1; this.color2 = col2;
+		this.color3 = col3; this.color4 = col4;
+		this.uoh = underlineOrHighlight;
+		// it doesn't matter that this isn't accurate. We're just using
+		// it for a proportion calculation, so as long as the relative
+		// size is right the absolute size doesn't matter.
+		this.totalTextWidth = 	textWidth(this.txt1) + 
+								textWidth(this.txt2) + 
+								textWidth(this.txt3) + 
+								textWidth(this.txt4); 
+		this.proportions = [textWidth(this.txt1) / this.totalTextWidth,
+							textWidth(this.txt2) / this.totalTextWidth,
+							textWidth(this.txt3) / this.totalTextWidth,
+							textWidth(this.txt4) / this.totalTextWidth];
+		this.rects = [];
+		for (let i = 0; i < 4; i++) {
+			let o = 0; // o = x position offset.
+			for (let n = 0; n < i; n++) { o += this.proportions[i - n - 1] * this.dims.x }
+			let oVect = createVector(o, 0);
+			oVect.add(this.pos);
+			this.rects.push(new content_rect(	this.proportions[i] * this.dims.x,
+												this.dims.y,
+												oVect,
+												this.texts[i],
+												false,
+												"",
+												this.color1, this.color2,
+												this.color3, this.color4,
+												this.uoh));
+		}
+	}
+	place() {
+		for (let index in this.rects) {
+			let o = 0; // o = position relative to what rect it is.
+			for (let n = 0; n < index; n++) { o += this.proportions[index - n - 1] * this.dims.x }
+			let oVect = createVector(o, 0);
+			oVect.add(this.pos);
+			this.rects[index].mod = true;
+			this.rects[index].pos = oVect;
+			this.rects[index].place();
+			if (index < 4) {
+				strokeWeight(5*tw);
+				line(oVect.x + this.proportions[index + 1] / 2, oVect.y,
+					oVect.x + this.proportions[index + 1] / 2, oVect.y + this.dims.y / 3);
+			}
+		}
+	}
+	testHover() {
+		for (let index in this.rects) {
+			this.rects[index].testHover();
+		}
+	}
+}
+// END META CONTAINER
